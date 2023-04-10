af06e067361c52c9a6557bb230c79fd7e9b99be8
Chris Kalafarski <chris@farski.com>
2023-04-10 17:51:37 -0400

    Modernize tooling

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 
 exports.handler = async function handler(event) {
   const qsp = event.queryStringParameters;
 
   const gcs = google.customsearch({
-    version: 'v1',
+    version: "v1",
     auth: process.env.CSE_API_KEY,
   });
 
   const results = await gcs.cse.siterestrict.list({
     cx: process.env.ENGINE_ID,
-    q: !qsp.l || qsp.l !== 'all' ? `${qsp.q} more:${qsp.l}`: (qsp.q),
+    q: !qsp.l || qsp.l !== "all" ? `${qsp.q} more:${qsp.l}` : qsp.q,
     ...(qsp.s && { start: qsp.s }),
-    ...(qsp.t && { sort: qsp.t })
+    ...(qsp.t && { sort: qsp.t }),
   });
 
   return {
     statusCode: 200,
     headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS',
-        'Access-Control-Allow-Origin': '*'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers":
+        "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "GET,OPTIONS",
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify(results.data),
   };
